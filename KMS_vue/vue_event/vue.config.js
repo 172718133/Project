65008c1a3b0f9abe07f43bbebe173447abba4590
YiLong <2488174858@qq.com>
2022-11-29 13:57:33 +0800

    publicPath的使用

--- KMS_vue/vue_event/vue.config.js
+++ KMS_vue/vue_event/vue.config.js
@@ -2,5 +2,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './'
+  // 影响打包时的路径
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
 })
