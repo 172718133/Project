e0af0402090aeee0a7e70f1fd674dec96f79a9ef
YiLong <2488174858@qq.com>
2022-11-29 13:45:58 +0800

    取消打包瘦身

--- KMS_vue/vue_event/vue.config.js
+++ KMS_vue/vue_event/vue.config.js
@@ -2,21 +2,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // 影响打包时的路径
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-  // 排除以下文件夹，打包时不打包这些文件夹
-  configureWebpack: {
-    externals: {
-      // 基本格式：
-      // '包名' : '在项目中引入的名字'
-      vue: 'Vue',
-      'vue-router': 'VueRouter',
-      vuex: 'Vuex',
-      axios: 'axios',
-      dayjs: 'dayjs',
-      'element-ui': 'ELEMENT',
-      'vue-quill-editor': 'VueQuillEditor',
-      'vuex-persistedstate': 'createPersistedState'
-    }
-  }
+  publicPath: './'
 })
